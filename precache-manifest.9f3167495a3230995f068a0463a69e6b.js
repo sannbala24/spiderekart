2255b62d49826071b639eb705e783bef89cfcf50
sannbala24 <sannbala24@gmail.com>
2020-10-01 01:59:09 +0530

    Updates

---
+++ precache-manifest.9f3167495a3230995f068a0463a69e6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8078738f348bef67dee59b953f33f9ca",
+    "url": "/spiderekart/index.html"
+  },
+  {
+    "revision": "5763afe6e9016e95fada",
+    "url": "/spiderekart/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "570442f5021f32ed5087",
+    "url": "/spiderekart/static/css/main.26e17ced.chunk.css"
+  },
+  {
+    "revision": "5763afe6e9016e95fada",
+    "url": "/spiderekart/static/js/2.2472cf7a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/spiderekart/static/js/2.2472cf7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "570442f5021f32ed5087",
+    "url": "/spiderekart/static/js/main.bb8c8f69.chunk.js"
+  },
+  {
+    "revision": "5b1d1d0c5eb225a9b40c",
+    "url": "/spiderekart/static/js/runtime-main.be5393c0.js"
+  }
+]);
